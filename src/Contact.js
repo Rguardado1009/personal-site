8682d2dc461ea8a06b3ef0536eb2a022c785e2e3
Rguardado1009 <rguardado1009@gmail.com>
2022-01-30 23:05:27 -0500

    added contact info

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,24 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="contact_page">
-      <h1> this is the contact page</h1>
+      <img
+        style={{ width: "60px" }}
+        src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+        alt=""
+      />
+      <h1> Github </h1>
+      <img
+        style={{ width: "60px" }}
+        src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png"
+        alt=""
+      />
+      <h1> LinkedIn </h1>
+      <img
+        style={{ width: "60px" }}
+        src="https://7xhqfps15y-flywheel.netdna-ssl.com/wp-content/uploads/2021/01/Twitter-Logo.png"
+        alt=""
+      />
+      <h1> Twitter </h1>
     </div>
   );
 }
