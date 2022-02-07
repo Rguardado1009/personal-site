ef8e1f0679b532e3aaabd9f5020bac2ff7baa174
Rguardado1009 <rguardado1009@gmail.com>
2022-02-06 19:41:52 -0500

    updated project & portfolio css/components

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,17 @@
 import React from "react";
-
-function Portfolio({ imgUrl }) {
+import "./Portfolio.css";
+function Portfolio({ imgUrl, title }) {
   return (
     <div className="project-container_portfolio">
       <div className="project-container_portfolio-image"></div>
       <img src={imgUrl} alt="project" />
+      <div className="portfolio-container_porfolio-content">
+        <div>
+          <h3>{title}</h3>
+        </div>
+        <p>View Code</p>
+        <p>Live Site</p>
+      </div>
     </div>
   );
 }
